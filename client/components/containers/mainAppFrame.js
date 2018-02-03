4221fd91fac5f03e02a746905484ab54e7abbc2b
Robert David McLeod <robertdavid01@gmail.com>
2018-02-03 13:52:58 +0100

    More basic file structure, repo testing.

---
+++ client/components/containers/mainAppFrame.js
@@ -0,0 +1,44 @@
+// mainAppFrame.js
+
+if (Meteor.isClient) {
+
+	Template.mainAppFrame.hooks({
+
+		created: function () {
+			var self = this;
+
+			self.initialized = false;
+
+			// Init App
+			self.initializeApp = function () {
+				Session.setDefault({
+					// Set session defaults
+				});
+
+				// Do other initializations
+
+				// Set the app initialized state
+				self.initialized = true;
+			};
+
+			self.autorun(function () {
+
+				// If first run, call initialization method
+				if (!self.initialized) {
+					self.initializeApp();
+				}
+
+			});
+
+		}
+
+	});
+
+	Template.mainAppFrame.helpers({
+
+		appFrameHelper: function () {
+		}
+
+	});
+
+}
---
+++ client/components/modules/tasksList/tasksList.js
@@ -0,0 +1,10 @@
+// tasksList.js
+
+if (Meteor.isClient) {
+
+	Template.tasksList.onCreated(function () {
+		var self = this;
+	});
+
+
+}
